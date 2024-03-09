b48ab6b89be0b1976d6e8d0ac116aaa197d1ae14
ShinzenATT <alen.mukaca@gmail.com>
2024-03-10 00:11:39 +0100

    Mocked up the main view

--- app-new/src/main/java/se/gorymoon/hdopen/MainActivity.kt
+++ app-new/src/main/java/se/gorymoon/hdopen/MainActivity.kt
@@ -3,27 +3,88 @@ package se.gorymoon.hdopen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults.exitUntilCollapsedScrollBehavior
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import se.gorymoon.hdopen.ui.theme.HDOpenTheme
+import se.gorymoon.hdopen.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HDOpenTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            HDOpenScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HDOpenScreen() {
+    HDOpenTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(
+                        text = "HDOpen",
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    ) },
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    actions = {
+                        IconButton(
+                            onClick = { /*TODO open settings*/ },
+                            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.onPrimaryContainer)
+                        ) {
+                            Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                Image(painter = painterResource(R.drawable.dumheter_iin), contentDescription = "Ad")
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {/* TODO Add action*/}) {
+                    Icon(Icons.Filled.Refresh, "Refresh")
+                }
+            }
+        ) {
+            // A surface container using the 'background' color from the theme
+            Surface(color = MaterialTheme.colorScheme.tertiaryContainer, modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(it),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Open")
+                    Text("For a while")
                 }
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ScreenPreview() {
+    HDOpenScreen()
 }
 
 @Composable
@@ -32,12 +93,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HDOpenTheme {
-        Greeting("Android")
-    }
 }
