4501ec5d48641749d3d193fd2dfc52828c709a94
ShinzenATT <alen.mukaca@gmail.com>
2024-03-13 20:44:43 +0100

    Added ktor dependencies

--- app-new/build.gradle.kts
+++ app-new/build.gradle.kts
@@ -1,3 +1,5 @@
+val ktor_version: String by project
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -56,6 +58,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+    implementation("io.ktor:client-serialization-json-kotlinx:$ktor_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
